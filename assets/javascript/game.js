e7fec4be89674ffad9a5ade604ea5fa2c14a9f31
Jadelet <slaethe@gmail.com>
2018-09-19 17:53:05 -0400

    created mechanisms to click gems and compare scores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 // Computer selected solution 
 
 // vars to hold random values for gemstones
-var crystalz =""
+var crystalz =0
 
 
 // Game counters
 var winsCounter = 0;
 var lossesCounter = 0;
 
-//variable to hold values for computer generated random number
-var numberz = ""
+//variable to hold values for computer generated random numbers
+var numberz = 0
+var playertotal = 0
 
 
 
@@ -32,6 +33,13 @@ $("#randomnumber").text(numberz);
 $("#gemRow").click(function() {
 crystalz = Math.floor((Math.random() * 12) + 1);
 console.log("Your crystal vlaue is " + crystalz);
-
+playertotal= playertotal + crystalz;
+console.log("Player total is now " + playertotal);
+if (playertotal===numberz) {
+  console.log ("winner, winner, chicken dinner!");
+}
+  else if (playertotal>numberz) { 
+    console.log ("LOSER!");
+}
 });
 
