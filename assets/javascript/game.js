b97bb00a403015eff1a02b023926db9dd69db83a
Jadelet <slaethe@gmail.com>
2018-09-18 23:24:51 -0400

    updated HTML with more pictures, variables, started pseudocode and a tiny bit of actual code for random number gen

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Computer selected solution 
+var randomNumberValue = ()
+// vars to hold random values for gemstones
+var diamondValue = ()
+var emeraldValue = ()
+var rubyValue = ()
+var sapphireValue = ()
+
+// vars to pull html elements to be manipulated?
+
+// Game counters
+var winsCounter = 0;
+var lossesCounter = 0;
+
+
+// generate random number between 19-120
+
+function= randomNumberator(){
+  Math.floor(Math.random() * 100)+20;
+  console.log()
+
+
+
+// Document Ready event before jquery entries
+
+$(document).ready(function(){
+
+  // jQuery methods go here...
+
+});
