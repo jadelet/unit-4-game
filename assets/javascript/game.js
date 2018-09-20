f718b60cdffd52ce67be95f8b70f97ade57be69d
Jadelet <slaethe@gmail.com>
2018-09-19 23:25:13 -0400

    everything but the kitchen sink. All code working, now possibly more css styling to make it more responsive.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,124 +16,103 @@ var numberz = 0
 var playertotal = 0
 
 
-function restartGame(){
-numberz = Math.floor((Math.random() * 100 ) + 20);
-console.log("Your random number is " + numberz);
-$("#randomnumber").text(numberz);
-playertotal = 0
-$("#crystal-value").text(playertotal)
+function restartGame() {
+  numberz = Math.floor((Math.random() * 100) + 20);
+  console.log("Your random number is " + numberz);
+  $("#randomnumber").text(numberz);
+  playertotal = 0
+  $("#crystal-value").text(playertotal)
+}
+function changeScore() {
+    if (playertotal === numberz) {
+    console.log("winner, winner, chicken dinner!");
+
+    $("#winsHTML").html(winsCounter + 1);
+    $(restartGame())
+  } 
+  else if (playertotal > numberz) {
+    console.log("LOSER!");
+    
+
+    $("#lossesHTML").html(lossesCounter + 1);
+    $(restartGame())
+  }
+  
 }
 
 // Document Ready event before jquery entries
 
-$(restartGame()) 
- 
-console.log("Test")
-  // jQuery methods go here...
+$(restartGame())
 
-  // Computer selected solution 
-numberz = Math.floor((Math.random() * 100 ) + 20);
+console.log("Test")
+// jQuery methods go here...
+
+// Computer selected solution 
+numberz = Math.floor((Math.random() * 100) + 20);
 console.log("Your random number is " + numberz);
 $("#randomnumber").text(numberz);
 
 
-var emeraldClicked=false
-var sapphireClicked=false
-var diamondClicked=false
-var rubyClicked=false
+var emeraldClicked = false
+var sapphireClicked = false
+var diamondClicked = false
+var rubyClicked = false
 
 
-$("#gem1").click(function() {
-  console.log ("test2")
-  if (emeraldClicked ===false) {
+$("#gem1").click(function () {
+  console.log("test2")
+  if (emeraldClicked === false) {
     emerald = Math.floor((Math.random() * 12) + 1);
     console.log("Your crystal value is " + emerald);
   };
-    playertotal= playertotal + emerald;
-    console.log("Player total is now " + playertotal);
-    $("#crystal-value").text(playertotal); 
-    emeraldClicked=true
-  
-if (playertotal===numberz) {
-  console.log ("winner, winner, chicken dinner!");
- 
-  $("#winsHTML").html(winsCounter+1); 
-}
-  else if (playertotal>numberz) { 
-    console.log ("LOSER!");
-  
-    $("#lossesHTML").html(lossesCounter+1);
-  }
-    
+  playertotal = playertotal + emerald;
+  console.log("Player total is now " + playertotal);
+  $("#crystal-value").text(playertotal);
+  emeraldClicked = true;
+  changeScore();
+
   // restartGame();
 });
 
 
-if (sapphireClicked===false) {
-  $("#gem2").click(function() {
-var sapphire = Math.floor((Math.random() * 12) + 1);
- var sapphireClicked=true 
- console.log("Your crystal value is " + sapphire);
-  playertotal= playertotal + sapphire;
+$("#gem2").click(function () {
+  console.log("test3")
+  if (sapphireClicked === false) {
+    sapphire = Math.floor((Math.random() * 12) + 1);
+    console.log("Your crystal value is " + sapphire);
+  };
+  playertotal = playertotal + sapphire;
   console.log("Player total is now " + playertotal);
-  $("#crystal-value").text(playertotal); 
-  if (playertotal===numberz) {
-    console.log ("winner, winner, chicken dinner!");
-   
-    $("#winsHTML").html(winsCounter+1); 
-  }
-    else if (playertotal>numberz) { 
-      console.log ("LOSER!");
-    
-      $("#lossesHTML").html(lossesCounter+1);
-    }
-    
-    restartGame();
-  });
-  }
-  if (diamondClicked===false) {
-  $("#gem3").click(function() {
-    var diamond = Math.floor((Math.random() * 12) + 1);
-    var diamondClicked=true 
+  $("#crystal-value").text(playertotal);
+  sapphireClicked = true;
+  changeScore();
+
+});
+
+
+$("#gem3").click(function () {
+  console.log("test4")
+  if (diamondClicked === false) {
+    diamond = Math.floor((Math.random() * 12) + 1);
     console.log("Your crystal value is " + diamond);
-    playertotal= playertotal + diamond;
-    console.log("Player total is now " + playertotal);
-    $("#crystal-value").text(playertotal); 
-    if (playertotal===numberz) {
-      console.log ("winner, winner, chicken dinner!");
-     
-      $("#winsHTML").html(winsCounter+1); 
-    }
-      else if (playertotal>numberz) { 
-        console.log ("LOSER!");
-      
-        $("#lossesHTML").html(lossesCounter+1);
-      }
-    
-        restartGame();
-      });
-    }
-    if (rubyClicked===false) {
-    $("#gem4").click(function() {
-      var ruby = Math.floor((Math.random() * 12) + 1);
-      var rubyClicked = true
+  };
+  playertotal = playertotal + diamond;
+  console.log("Player total is now " + playertotal);
+  $("#crystal-value").text(playertotal);
+  diamondClicked = true;
+  changeScore();
+
+});
+
+$("#gem4").click(function () {
+  console.log("test5")
+if (rubyClicked === false) {
+      ruby = Math.floor((Math.random() * 12) + 1);
       console.log("Your crystal value is " + ruby);
-      playertotal= playertotal + ruby;
-      console.log("Player total is now " + playertotal);
-      $("#crystal-value").text(playertotal); 
-      if (playertotal===numberz) {
-        console.log ("winner, winner, chicken dinner!");
-       
-        $("#winsHTML").html(winsCounter+1); 
-      }
-        else if (playertotal>numberz) { 
-          console.log ("LOSER!");
-        
-          $("#lossesHTML").html(lossesCounter+1);
-        }
-    
-        restartGame();
-      });
-      }
-
-
+    }; 
+    playertotal = playertotal + ruby; 
+    console.log("Player total is now " + playertotal); 
+    $("#crystal-value").text(playertotal); 
+    rubyClicked = true; 
+    changeScore();
+  });
